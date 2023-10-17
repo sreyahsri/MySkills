fc345c692adbd7eb60453050dbb2c82a976f86fd
SATHI SREYAHSRI <86695494+sreyahsri@users.noreply.github.com>
2023-10-17 08:01:12 +0530

    Create P1.c

---
+++ Coding/C/Lab/Lab1/P1.c
@@ -0,0 +1,10 @@
+/* Write a program in 'C' to output as: */
+
+#include<stdio.h>
+int main()
+{
+  printf("#include<stdio.h> \n int main() \n \n\n\t");
+  printf("printf" "(\"Hello World \\n \"); \n\t");
+  printf("return 0; \n\n}\n");
+  return 0;
+}
